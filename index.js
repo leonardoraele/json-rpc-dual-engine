c8afbd72397d7fcd2b8d10bae430f12bcc3b0a31
Leonardo Raele <leonardoraele@gmail.com>
2021-05-11 10:54:43 -0300

    [FIX] [REF]Removed JsonRpcStream from the  main import file because it imports node stream and it would break in browser environments

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 module.exports.JsonRpcServer = require('./server.js');
 module.exports.JsonRpcClient = require('./client.js');
-module.exports.JsonRpcStream = require('./stream.js');
 module.exports.JsonRpcDualEngine = require('./lib/json-rpc/json-rpc-dual-engine.js');
