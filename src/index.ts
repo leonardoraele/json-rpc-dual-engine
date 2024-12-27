6ffa533dd7df80bd26178c066c037abae844bcaf
Leonardo Raele <leonardoraele@gmail.com>
2024-12-27 16:49:57 -0300

    feat: added index.js import

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { JsonRpcDualEngine } from './json-rpc/json-rpc-dual-engine.js';
+export { JsonRpcClient } from './json-rpc/json-rpc-client.js';
+export { JsonRpcServer } from './json-rpc/json-rpc-server.js';
